ea3bad8dab2ed08e333a67015155d7feb5e18559
Gauthier Delamarre <gde@opcoding.eu>
2018-04-10 16:52:31 +0200

    Update Config directive

--- src/Config/PhtmlLayoutPath.php
+++ src/Config/PhtmlLayoutPath.php
@@ -11,10 +11,11 @@ namespace ObjectivePHP\Middleware\Action\PhtmlAction\Config;
 
 use ObjectivePHP\Config\Directive\AbstractScalarDirective;
 use ObjectivePHP\Config\Directive\MultiValueDirectiveInterface;
+use ObjectivePHP\Config\Directive\MultiValuesHandlingTrait;
 
 class PhtmlLayoutPath extends AbstractScalarDirective implements MultiValueDirectiveInterface
 {
-
     const KEY = 'phtml.layout.path';
+
     protected $key = self::KEY;
 }
